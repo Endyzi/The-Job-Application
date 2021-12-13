60d96d42ffaf84c669c5c17c1d3f98ea4917e30e
Endyzi <34267140+Endyzi@users.noreply.github.com>
2021-12-13 11:42:55 +0100

    Add files via upload

---
+++ StepModel.js
@@ -0,0 +1,45 @@
+﻿var Step1Model = function () {
+    var self = this;
+    self.Email = '';
+    self.FullName = '';
+    self.Age = '';
+    self.Address = '';
+    self.Zip = '';
+    self.City = '';
+    self.Phone = '';
+    self.Homepage = '';
+
+    self.Initialized = false;
+}
+
+var Step2Model = function () {
+    var self = this;
+    self.WorkCurrent = '';
+    self.WorkCurrentDescription = '';
+    self.Experience = '';
+
+    self.Initialized = false;
+}
+
+var Step3Model = function () {
+    var self = this;
+    self.Education = '';    
+    self.PhotoExperience = '';
+
+    self.Initialized = false;
+}
+
+var Step4Model = function () {
+    var self = this;
+    self.Swedish = '';
+    self.Car = '';
+    self.DriversLicense = '';
+    self.CostumerExperience = '';
+    self.YouthExperience = '';
+    self.FileProfileImage = '';
+    self.FilePersonalLetter = '';
+    self.FileCv = '';
+    self.PreferredWorkplaces = [];
+
+    self.Initialized = false;
+}
